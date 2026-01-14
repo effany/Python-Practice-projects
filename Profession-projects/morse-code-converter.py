f5785b1d404063d1844ec0edaf302677269adfcf
effany <effany28119@gmail.com>
2026-01-14 11:15:06 +0100

    profession day 1

---
+++ Profession-projects/morse-code-converter.py
@@ -0,0 +1,40 @@
+MORSE_CODE = {
+    'A': '.-',
+    'B': '-...',
+    'C': '-.-.',
+    'D': '-..',
+    'E': '.',
+    'F': '..-.',
+    'G': '--.',
+    'H': '....',
+    'I': '..',
+    'J': '.---',
+    'K': '-.-',
+    'L': '.-..',
+    'M': '--',
+    'N': '-.',
+    'O': '---',
+    'P': '.--.',
+    'Q': '--.-',
+    'R': '.-.',
+    'S': '...',
+    'T': '-',
+    'U': '..-',
+    'V': '...-',
+    'W': '.--',
+    'X': '-..-',
+    'Y': '-.--',
+    'Z': '--..',
+}
+
+text = input("Enter text to convert to Morse code: ").upper().replace(' ', '')
+
+morse_code = []
+
+for i in text:
+    morse_code.append(MORSE_CODE[i])
+
+delimiter = " "
+final_morse = delimiter.join(morse_code)
+
+print(f"Your morse code is {final_morse}")
